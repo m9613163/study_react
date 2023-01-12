2e99a08b1d6f2260d6d9edc543193d63a2301f42
Hong Sunki <m9613163@gmail.com>
2023-01-12 20:40:52 +0900

    ch03 컴포넌트 CSS 스타일링) daisyui CSS 컴포넌트 이해하기 ~ 모달 컴포넌트 구현하기

--- ch03/ch03_5/src/pages/ButtonTest/IconTest.tsx
+++ ch03/ch03_5/src/pages/ButtonTest/IconTest.tsx
@@ -1,8 +1,16 @@
-export default function CopyMe() {
+import { Icon } from "../../theme/daisyui"
+
+export default function IconTest() {
+    const onClick = () => alert('Icon clicked')
     return (
         <section className='mt-4'>
-            <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-            <div className='mt-4'></div>
+            <h2 className="text-3xl font-bold text-center">IconTest</h2>
+            <div className='flex items-center justify-around mt-4'>
+                <Icon className="btn-primary btn-lg" name="settings" onClick={onClick} />
+                <Icon className="btn-secondary btn-md" name="done" onClick={onClick} />
+                <Icon className="btn-accent btn-sm" name="menu" onClick={onClick} />
+                <Icon className="btn-success btn-xs" name="file_upload" onClick={onClick} />
+            </div>
         </section>
     )
 }
