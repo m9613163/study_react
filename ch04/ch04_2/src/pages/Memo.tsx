7e85eb123dfac7135b6975cc7a90b9c23efcdbf3
Hong Sunki <m9613163@gmail.com>
2023-02-05 21:37:34 +0900

    ch04 함수 컴포너트와 리액트 훅) 04-2 useMemo와 useCallback 훅 이해하기 ~ 콜백 함수를 캐시하는 useCallback 훅

--- ch04/ch04_2/src/pages/Memo.tsx
+++ ch04/ch04_2/src/pages/Memo.tsx
@@ -1,8 +1,46 @@
-export default function CopyMe() {
+import { useMemo } from 'react'
+import { Title, Avatar } from '../components'
+import * as D from '../data'
+
+export default function Memo() {
+    // prettier-ignore
+    const headTexts = useMemo<string[]>(() => [
+        'No.', 'Name', 'Job Title', 'Email Adress'
+    ], [])
+    const users = useMemo<D.IUser[]>(() => D.makeArray(100).map(D.makeRandomUser), [])
+
+    const head = useMemo(
+        () => headTexts.map(text => <th key={text}>{text}</th>),
+        [headTexts]
+    )
+
+    const body = useMemo(
+        () =>
+            users.map((user, index) => (
+                <tr key={user.uuid}>
+                    <th>{index + 1}</th>
+                    <td className='flex items-center'>
+                        <Avatar src={user.avatar} size='1.5rem' />
+                        <p className='ml-2'>{user.name}</p>
+                    </td>
+                    <td>{user.jobTitle}</td>
+                    <td>{user.email}</td>
+                </tr>
+            )),
+            [users]
+    )
+
     return (
-        <section className='mt-4'>
-            <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-            <div className='mt-4'></div>
-        </section>
+        <div className='mt-4'>
+            <Title>Memo</Title>
+            <div className='p-4 mt-4 overflow-x-auto'>
+                <table className='table w-full table-zebra table-compact'>
+                    <thead>
+                        <tr>{head}</tr>
+                    </thead>
+                    <tbody>{body}</tbody>
+                </table>
+            </div>
+        </div>
     )
 }
